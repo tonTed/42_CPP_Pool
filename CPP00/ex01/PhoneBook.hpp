b777598dbde0280a28479c24c33793b5001b175c
tonted <yddetco@gmail.com>
2022-12-02 09:10:04 -0500

    :art: Change structure of folder and add all CPP modules in same folder!

---
+++ CPP00/ex01/PhoneBook.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 20:25:07 by tonted            #+#    #+#             */
+/*   Updated: 2022/12/01 20:53:02 by tonted           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class PhoneBook {
+	Contact	contacts[8];
+
+public:
+	PhoneBook(void);
+	~PhoneBook();
+};
+
+#endif
