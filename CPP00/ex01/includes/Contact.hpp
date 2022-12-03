a1a2976d70ea3355f144395bc47ae44c2997645f
tblanco <tblanco@student.42quebec.com>
2022-12-02 20:40:17 -0500

    :sparkles: [CPP00/ex01] add attributes and methods in contact!

--- CPP00/ex01/includes/Contact.hpp
+++ CPP00/ex01/includes/Contact.hpp
@@ -3,17 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 20:46:19 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/01 20:53:04 by tonted           ###   ########.fr       */
+/*   Updated: 2022/12/02 19:22:44 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iostream"
+
 class Contact {
 
+private:
+	std::string	_first_name;
+	std::string _last_name;
+	std::string _nickname;
+	std::string _phone_number;
+	std::string _darkest_secret;
+
 public:
-	Contact(/* args */);
+	Contact();
 	~Contact();
+
+	void	set_first_name(std::string first_name);
+	void	set_last_name(std::string last_name);
+	void	set_nickname(std::string nickname);
+	void	set_phone_number(std::string phone_number);
+	void	set_darkest_secret(std::string darkest_secret);
 };
 
--- CPP00/ex01/src/Contact.cpp
+++ CPP00/ex01/src/Contact.cpp
@@ -3,19 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 20:45:47 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/01 20:46:11 by tonted           ###   ########.fr       */
+/*   Updated: 2022/12/02 19:22:42 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-Contact::Contact(/* args */)
+Contact::Contact()
 {
 }
 
 Contact::~Contact()
 {
+}
+
+void	Contact::set_first_name(std::string first_name){
+	this->_first_name = first_name;
+}
+
+void	Contact::set_last_name(std::string last_name){
+	this->_last_name = last_name;
+}
+
+void	Contact::set_nickname(std::string nickname){
+	this->_nickname = nickname;
+}
+
+void	Contact::set_phone_number(std::string phone_number){
+	this->_phone_number = phone_number;
+}
+
+void	Contact::set_darkest_secret(std::string darkest_secret){
+	this->_darkest_secret = darkest_secret;
 }
