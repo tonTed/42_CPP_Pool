83c9d2501152ef695e6fe2350e8476ac2ce2ccb6
tblanco <tblanco@student.42quebec.com>
2022-12-02 20:41:58 -0500

    :construction: [CPP00/ex01] Implementation of add_contact in progress

--- CPP00/ex01/includes/PhoneBook.hpp
+++ CPP00/ex01/includes/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 20:25:07 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/02 14:25:56 by tblanco          ###   ########.fr       */
+/*   Updated: 2022/12/02 20:30:15 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,20 @@
 #define	ADD "ADD"
 #define	SEARCH "SEARCH"
 #define	EXIT "EXIT"
+#define	EXIT2 "EXIT"
 
 class PhoneBook {
 
 private:
-	Contact	contacts[8];
+	Contact	_contacts[8];
+	int		_i;
 	
 	void	_work(void);
 
 public:
 	PhoneBook(void);
 	~PhoneBook(void);
+	void	add_contact(void);
 };
 
 #endif
--- CPP00/ex01/src/PhoneBook.cpp
+++ CPP00/ex01/src/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 20:39:03 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/02 14:27:30 by tblanco          ###   ########.fr       */
+/*   Updated: 2022/12/02 20:34:53 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,31 @@
 
 PhoneBook::PhoneBook(){
 	this->_work();
+	this->_i = 0;
 }
 
 PhoneBook::~PhoneBook(){
 
+}
+
+void	PhoneBook::add_contact(void){
+	
+	std::string	input;
+	
+	std::cout << "First Name: "; std::getline(std::cin, input);
+	this->_contacts[this->_i].set_first_name(input);
+	std::cout << "Last Name: "; std::getline(std::cin, input);
+	this->_contacts[this->_i].set_last_name(input);
+	std::cout << "Nickname: "; std::getline(std::cin, input);
+	this->_contacts[this->_i].set_nickname(input);
+	std::cout << "Phone Number: ";std::getline(std::cin, input);
+	this->_contacts[this->_i].set_phone_number(input);
+	std::cout << "Darkest Secret: "; std::getline(std::cin, input);
+	this->_contacts[this->_i].set_darkest_secret(input);
+	if (this->_i == 7)
+		this->_i = 0;
+	else
+		this->_i++;
 }
 
 void	PhoneBook::_work(void){
@@ -25,14 +46,15 @@ void	PhoneBook::_work(void){
 
 	while (42)
 	{
-		std::cin >> input;
+		std::getline(std::cin, input);
 		if (input == ADD)
-			std::cout << input << std::endl;
+			this->add_contact();
 		else if (input == SEARCH)
 			std::cout << input << std::endl;
 		else if (input == EXIT)
 			break ;
 		else
 			std::cout << "command not exists" << std::endl;
+		// break;
 	}
 }
