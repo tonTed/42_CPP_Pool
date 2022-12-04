389979e258e80716b9bf8f906ebd870c8bf80c42
tonted <yddetco@gmail.com>
2022-12-03 20:44:09 -0500

    :tada: [CPP00/ex02] Begin exercice!

---
+++ CPP00/ex02/src/Account.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 20:26:38 by tonted            #+#    #+#             */
+/*   Updated: 2022/12/03 20:43:31 by tonted           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ){
+
+}
+
+Account::~Account( void ){
+
+}
+
+int		Account::getNbAccounts( void ){
+
+}
+
+int		Account::getTotalAmount( void ){
+
+}
+
+int		Account::getNbDeposits( void ){
+
+}
+
+int		Account::getNbWithdrawals( void ){
+
+}
+
+void	Account::displayAccountsInfos( void ){
+
+}
+
+void	Account::makeDeposit( int deposit ){
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal ){
+
+}
+
+int		Account::checkAmount( void ) const{
+
+}
+
+void	Account::displayStatus( void ) const{
+
+}
+
+void	Account::_displayTimestamp( void ){
+
+}
+
+
