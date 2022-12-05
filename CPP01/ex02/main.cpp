bc56241e0b70c9b861de9606879e6fcc2e4c2ccf
tblanco <tblanco@student.42quebec.com>
2022-12-05 14:14:26 -0500

    :bug: [CPP01/ex02] Replace std::cout instead printf 😡

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:56:10 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/04 15:10:14 by tonted           ###   ########.fr       */
+/*   Updated: 2022/12/05 14:13:28 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,16 @@ int main( void )
 	
 	std::string	&stringREF = string;
 
-	printf("string    address: %p\n", &string);
-	printf("stringPTR address: %p\n", stringPTR);
-	printf("stringREF address: %p\n", stringREF.c_str());
+
+	std::cout << "string    address: " << &string << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
 
 	std::cout << std::endl;
 
-	std::cout << "string    value: " + string << std::endl;
-	std::cout << "stringPTR value: " + *stringPTR << std::endl;
-	std::cout << "stringREF value: " + stringREF << std::endl;
+	std::cout << "string    value: " << string << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
 
 	return 0;
 }
