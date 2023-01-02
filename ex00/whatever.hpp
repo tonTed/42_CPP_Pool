75992fb3e68a92ef7e9f9707f1209fe89b1fbca5
tblanco <tblanco@student.42quebec.com>
2023-01-02 13:48:32 -0500

    :sparkles: [CPP07/ex00] min done!

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,15 +6,17 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 13:31:25 by tblanco           #+#    #+#             */
-/*   Updated: 2023/01/02 13:35:33 by tblanco          ###   ########.fr       */
+/*   Updated: 2023/01/02 13:46:18 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 template <typename T>
-void swap(T &a, T &b){
+void	swap(T &a, T &b){
 	T	tmp;
 	tmp = a;
 	a = b;
 	b = tmp;
 }
 
+template <typename T>
+T const &	min(T const &a, T const &b){return a < b ? a : b;}
