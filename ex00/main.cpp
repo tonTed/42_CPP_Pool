d4a009894bba65f8aaaba10d2123d081549a7624
tblanco <tblanco@student.42quebec.com>
2023-01-02 13:49:29 -0500

    :sparkles: [CPP07/ex00] max done!

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 13:33:21 by tblanco           #+#    #+#             */
-/*   Updated: 2023/01/02 13:47:06 by tblanco          ###   ########.fr       */
+/*   Updated: 2023/01/02 13:48:53 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int main( void ) {
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	
 	return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 13:31:25 by tblanco           #+#    #+#             */
-/*   Updated: 2023/01/02 13:46:18 by tblanco          ###   ########.fr       */
+/*   Updated: 2023/01/02 13:48:45 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,6 @@ void	swap(T &a, T &b){
 
 template <typename T>
 T const &	min(T const &a, T const &b){return a < b ? a : b;}
+
+template <typename T>
+T const &	max(T const &a, T const &b){return a > b ? a : b;}
