3429a096b598ac88f53a883f1ac7cca8c1721b53
tblanco <tblanco@student.42quebec.com>
2023-01-06 13:55:08 -0500

    :construction: [CPP06/ex00] trys with stringstream ! push for home

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,13 +6,14 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 08:19:51 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/23 18:01:08 by tblanco          ###   ########.fr       */
+/*   Updated: 2023/01/06 09:46:26 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Converter.hpp"
 #include <climits>
 #include <cfloat>
+#include <sstream>
 
 // void print_byte_as_bits(char val) {
 // 	for (int i = 7; 0 <= i; i--) {
@@ -29,7 +30,22 @@ int	main(int ac, char *av[]){
 		return 0;
 	}
 
-	Converter toConvert(av[1]);
+	// Converter toConvert(av[1]);
+
+	std::stringstream	ss;
+
+	int		_int;
+	char	_char;
+	float	_float;
+	double	_double;
+
+	ss << av[1];
+
+	ss >> _int;		std::cout << "int: " << _int << std::endl;
+	ss >> _char;	std::cout << "char: " << _char << std::endl;
+	ss >> _float;	std::cout << "float: " << _float << std::endl;
+	ss >> _double;	std::cout << "double: " << _double<< std::endl;
+
 	
 	return 0;
 }
--- CPP06/ex00/play.cpp
+++ CPP06/ex00/play.cpp
@@ -3,27 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   play.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 09:11:29 by tonted            #+#    #+#             */
-/*   Updated: 2022/12/30 09:15:21 by tonted           ###   ########.fr       */
+/*   Updated: 2023/01/06 13:51:27 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cfloat>
 #include <cmath>
+#include <ios>
 #include <iostream>
 #include <limits>
 #include <float.h>
+#include <sstream>
 
-int main()
+int main(int ac, char *av[])
 {
-    double max = std::numeric_limits<double>::max();
-    double inf = std::numeric_limits<double>::infinity();
+    // double max = std::numeric_limits<double>::max();
+    // double inf = std::numeric_limits<double>::infinity();
 
-    double  test = DBL_MAX + 1;
+    // double  test = DBL_MAX + 1;
 
-    std::cout << isinf(test) << std::endl;
+    // std::cout << isinf(test) << std::endl;
  
-    if(inf > max)
-        std::cout << inf << " is greater than " << max << '\n';
+    // if(inf > max)
+    //     std::cout << inf << " is greater than " << max << '\n';
+
+
+    std::stringstream	ss;
+
+	double	_double;
+
+	ss << av[1];
+    ss >> _double;
+    
+	int		_int = static_cast<int>(_double);
+	char	_char = static_cast<char>(_double);
+	float	_float= static_cast<float>(_double);
+
+		std::cout << "double: " << std::fixed <<  _double << std::endl;
+        std::cout << "int: " << std::fixed <<  _int << std::endl;
+        std::cout << "float: " << std::fixed <<  _float << std::endl;
+        std::cout << "char: " <<  std::fixed << _char << std::endl;
+
+
+
+    // std::cout << std::fixed << FLT_MAX << std::endl;
+    // std::cout << std::fixed << DBL_MAX << std::endl;
+    // std::cout << std::fixed << LDBL_MAX << std::endl;
+
 }
