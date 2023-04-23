0d4e656adce04ad3e42b383357d95246c466b35e
tonted <yddetco@gmail.com>
2023-04-22 23:48:23 -0400

    🎉 [CPP09/ex01] Begin ex01

---
+++ CPP09/ex01/src/RPN.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Teddy BLANCO on 2023-04-22.
+//
+
+#include "RPN.hpp"
---
+++ CPP09/ex01/src/RPN.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Teddy BLANCO on 2023-04-22.
+//
+
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include "../Log.hpp"
+
+class RPN {
+
+};
+
+
+#endif //RPN_HPP
