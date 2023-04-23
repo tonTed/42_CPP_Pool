3baafe81c1c01c38aa3647e70311e98d22849120
tonted <yddetco@gmail.com>
2023-04-23 11:50:08 -0400

    🎉 [CPP09/ex02] Begin ex02!

---
+++ CPP09/ex02/src/PmergeMe.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Teddy BLANCO on 2023-04-23.
+//
+
+#include "PmergeMe.hpp"
---
+++ CPP09/ex02/src/PmergeMe.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Teddy BLANCO on 2023-04-23.
+//
+
+#include "../Log.hpp"
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+
+class PmergeMe {
+
+};
+
+
+#endif //PMERGEME_HPP
